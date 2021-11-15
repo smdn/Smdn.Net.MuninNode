67b3437685a91954305ef2b48910f85293e1a739
smdn <smdn@smdn.jp>
2021-11-16 01:22:01 +0900

    add support for reporting UNKNOWN plugin field value

--- src/Smdn.Net.MuninNode/Smdn.Net.MuninPlugin/PluginField.cs
+++ src/Smdn.Net.MuninNode/Smdn.Net.MuninPlugin/PluginField.cs
@@ -8,15 +8,44 @@ namespace Smdn.Net.MuninPlugin {
   public readonly struct PluginField {
     public string ID { get; }
     public string Label { get; }
-    public double Value { get; }
+    /// <summary>A value for plugin field.</summary>
+    /// <remarks/>Reports 'UNKNOWN' as a plugin field value if <see cref="Value"/> is <see langword="null"/>.<remarks>
+    public double? Value { get; }
     public string GraphStyle { get; }
+
+    internal string FormattedValueString => Value.HasValue
+      ? Value.Value.ToString() // TODO: format specifier
+      : "U" /* UNKNOWN */;
+
+    public static PluginField CreateUnknownValueField(
+      string label,
+      string graphStyle = null
+    )
+      => new(
+        id: GetDefaultIDFromLabel(label),
+        label: label,
+        value: (double?)null,
+        graphStyle: graphStyle
+      );
+
+    public static PluginField CreateUnknownValueField(
+      string id,
+      string label,
+      string graphStyle = null
+    )
+      => new(
+        id: id,
+        label: label,
+        value: (double?)null,
+        graphStyle
+      );
 
     public PluginField(
       string label,
       double value,
       string graphStyle
     )
-      : this(GetDefaultIDFromLabel(label), label, value, graphStyle)
+      : this(GetDefaultIDFromLabel(label), label, (double?)value, graphStyle)
     {
     }
 
@@ -24,6 +53,16 @@ namespace Smdn.Net.MuninPlugin {
       string id,
       string label,
       double value,
+      string graphStyle = null
+    )
+      : this(id, label, (double?)value, graphStyle)
+    {
+    }
+
+    private PluginField(
+      string id,
+      string label,
+      double? value,
       string graphStyle = null
     )
     {
